1385758cee0dadb428f207245b56208c7584966e
dahasaln <dahasaln@mail.ru>
2023-03-09 19:05:34 +0300

    09.03.2023/ 1

--- api.py
+++ api.py
@@ -6,7 +6,7 @@ class PetFriends:
     def __init__(self):
         self.base_url = "https://petfriends.skillfactory.ru/"
 
-#1.Метод get:
+#1.
     def get_api_key(self, email, password) :
 
         headers = {
@@ -22,7 +22,7 @@ class PetFriends:
             result = res.text
         return status, result
 
-#2.Метод get_list_of_pets:
+#2.
     def get_list_of_pets(self, auth_key, filter):
 
         headers = {'auth_key': auth_key['key']}
@@ -61,7 +61,7 @@ class PetFriends:
             result = res.text
         return status, result
 
-
+#4.
     def delete_pets(self, auth_key: json, pet_id: str) -> json:
         '''Метод делает delete запрос к API сервера, удоляет питомца по его ID и возвращает
         статус запроса'''
@@ -79,7 +79,7 @@ class PetFriends:
         except:
             result = res.text
         return status, result
-
+#5.
     def update_pet_info(self, auth_key: json, pet_id: str, name: str, animal_type: str, age: str) ->json:
         headers = {
             'auth_key': auth_key['key'],
@@ -100,7 +100,7 @@ class PetFriends:
         except:
             result = res.text
         return status, result
-
+#6.
     def add_new_pet_without_photo(self, auth_key: json, name: str, animal_type: str, age: str) ->json:
         '''Метод делает post запрос к API сервера, добавляет новые данные из data на сайт
         и возвращает код статуса запроса и результат в формате json с информацией о животном.'''
@@ -122,7 +122,7 @@ class PetFriends:
         except:
             result = res.text
         return status, result
-
+#7.
     def add_photo_of_pet(self, auth_key: json, pet_id: str, pet_photo: str) ->json:
         '''Метод делает post запрос к API сервера и добавляет новое фото указанного
         pet_id питомца. Возвращает код статуса запроса и результат в формате json с информацией о животном.'''
@@ -146,44 +146,3 @@ class PetFriends:
         return status, result
 
 
-#
-#
-# def delete_pet(self, auth_key: json, pet_id: str) -> json:
-#     """Метод отправляет на сервер запрос на удаление питомца по указанному ID и возвращает
-#     статус запроса и результат в формате JSON с текстом уведомления о успешном удалении.
-#     На сегодняшний день тут есть баг - в result приходит пустая строка, но status при этом = 200"""
-#
-#     headers = {'auth_key': auth_key['key']}
-#
-#     res = requests.delete(self.base_url + 'api/pets/' + pet_id, headers=headers)
-#     status = res.status_code
-#     result = ""
-#     try:
-#         result = res.json()
-#     except json.decoder.JSONDecodeError:
-#         result = res.text
-#     return status, result
-#
-#
-# def update_pet_info(self, auth_key: json, pet_id: str, name: str,
-#                     animal_type: str, age: int) -> json:
-#     """Метод отправляет запрос на сервер о обновлении данных питомуа по указанному ID и
-#     возвращает статус запроса и result в формате JSON с обновлённыи данными питомца"""
-#
-#     headers = {'auth_key': auth_key['key']}
-#     data = {
-#         'name': name,
-#         'age': age,
-#         'animal_type': animal_type
-#     }
-#
-#     res = requests.put(self.base_url + 'api/pets/' + pet_id, headers=headers, data=data)
-#     status = res.status_code
-#     result = ""
-#     try:
-#         result = res.json()
-#     except json.decoder.JSONDecodeError:
-#         result = res.text
-#     return status, result
-#
-#
